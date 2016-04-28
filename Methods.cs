04b4a121ac3c125f48a7ce9fd985e1fe8a6757e7
nasun4o <nasunawow@abv.bg>
2016-04-28 13:41:54 +0300

    Homework

---
+++ Methods.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace Methods
+{
+    internal class Methods
+    {
+        private static double CalcTriangleArea(double sideA, double sideB, double sideC)
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Sides must be non negative number");
+            }
+
+            double s = (sideA + sideB + sideC) / 2;
+            double area = Math.Sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+
+            return area;
+        }
+
+        private static string NumberToDigit(int number)
+        {
+            switch (number)
+            {
+                case 0:
+                    return "zero";
+                case 1:
+                    return "one";
+                case 2:
+                    return "two";
+                case 3:
+                    return "three";
+                case 4:
+                    return "four";
+                case 5:
+                    return "five";
+                case 6:
+                    return "six";
+                case 7:
+                    return "seven";
+                case 8:
+                    return "eight";
+                case 9:
+                    return "nine";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(number), "Numbers  must be in this range");
+            }
+        }
+
+        private static int FindMax(params int[] elements)
+        {
+            if (elements == null || elements.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(elements), "Number cannot be null, and need atleast 2 elements in the array");
+            }
+
+            for (int i = 1; i < elements.Length; i++)
+            {
+                if (elements[i] > elements[0])
+                {
+                    elements[0] = elements[i];
+                }
+            }
+
+            return elements[0];
+        }
+
+        private static void PrintAsNumber(double number, string format)
+        {
+            switch (format)
+            {
+                case "f":
+                    Console.WriteLine("{0:f2}", number);
+                    break;
+                case "%":
+                    Console.WriteLine("{0:p0}", number);
+                    break;
+                case "r":
+                    Console.WriteLine("{0,8}", number);
+                    break;
+                default:
+                    throw new InvalidOperationException("Invalid operation");
+            }
+        }
+        
+        private static double CalcDistance(double x1, double y1, double x2, double y2,
+             out bool isHorizontal, out bool isVertical)
+        {
+            isHorizontal = (Equals(y1, y2));
+            isVertical = (Equals(x1, x2));
+
+            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+            return distance;
+        }
+
+        private static void Main()
+        {
+            Console.WriteLine(CalcTriangleArea(3, 4, 5));
+
+            Console.WriteLine(NumberToDigit(5));
+
+            Console.WriteLine(FindMax(5, -1, 3, 2, 14, 2, 3));
+
+            PrintAsNumber(1.3, "f");
+            PrintAsNumber(0.75, "%");
+            PrintAsNumber(2.30, "r");
+
+            bool horizontal, vertical;
+            Console.WriteLine(CalcDistance(3, -1, 3, 2.5, out horizontal, out vertical));
+            Console.WriteLine("Horizontal? " + horizontal);
+            Console.WriteLine("Vertical? " + vertical);
+
+            Student peter = new Student
+            {
+                FirstName = "Peter",
+                LastName = "Ivanov",
+                OtherInfo = "From Sofia, born at 1992.03.17"
+            };
+
+            Student stella = new Student
+            {
+                FirstName = "Stella",
+                LastName = "Markova",
+                OtherInfo = "From Vidin, gamer, high results, born at 1993.03.11"
+            };
+
+            Console.WriteLine("{0} older than {1} -> {2}",
+                peter.FirstName, stella.FirstName, peter.IsOlderThan(stella));
+        }
+    }
+}
